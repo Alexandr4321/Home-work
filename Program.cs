8985205d7b6e51fbf32514d86c3b73e7adac5e01
Alexander Vasilenko <vasilenkosaha4321@gmail.com>
2023-02-02 15:50:25 +0600

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿ int Digits()
+ {
+    
+    int num = Convert.ToInt32(Console.ReadLine());
+    int num1 = num / 100 ;
+    int num2= num - (num1*100);
+    int num3= num2 / 10;
+    return num3;
+
+ }
+ Console.Write("input your number: ");
+ int result = Digits();
+ Console.WriteLine($"second digit is {result}");
